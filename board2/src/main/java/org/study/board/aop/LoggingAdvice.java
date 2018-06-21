d1f14797c40e0d658344cf05fb26a5f081d9bc50
201-12 <201-12@201-12-PC>
2018-06-21 16:38:40 +0900

    180621

---
+++ board2/src/main/java/org/study/board/aop/LoggingAdvice.java
@@ -0,0 +1,18 @@
+package org.study.board.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAdvice {
+	private static final Logger logger = LoggerFactory.getLogger(LoggingAdvice.class);
+	
+	@Before("execution(* org.study.board.service.BoardServiceImpl.*(*))")
+	public void startLog() {
+		logger.info("startLog ...");
+	}
+}
